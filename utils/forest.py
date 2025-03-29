68a4c46bec5cc0261d9b73cea3fdca607b7ec27b
Pavlo Hilei <philei@tenstorrent.com>
2025-03-29 14:22:07 +0100

    Fix build and run instructions. Fix include and rel path issues

--- utils/forest.py
+++ utils/forest.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 import json
 
-train_data = pd.read_csv("../data/iris_train.csv")
+train_data = pd.read_csv("data/iris_train.csv")
 X_train = train_data.drop(columns=["Species"])
 y_train = train_data["Species"]
 
@@ -22,9 +22,9 @@ tree_structure = {
     "classes": label_encoder.classes_.tolist()
 }
 
-with open("forest.json", "w") as f:
+with open("data/forest.json", "w") as f:
     json.dump({"forest": tree_structure}, f, indent=4)
 
-X_test = pd.read_csv("../data/iris_test.csv")
+X_test = pd.read_csv("data/iris_test.csv")
 y_pred_encoded = model.predict(X_test)
 y_pred = label_encoder.inverse_transform(y_pred_encoded)
